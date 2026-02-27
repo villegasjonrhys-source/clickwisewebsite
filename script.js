841974debaa17a9f87573d5466c3a4ec3b787d96
villegasjonrhys-source <villegasjonrhys@gmail.com>
2026-02-27 09:38:08 +0800

    Add login function to handle user authentication

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function showMessage(){
 alert("Welcome to ClickWise!");
 }
+function login() {
+
+let username = document.querySelector('input[type="text"]').value;
+let password = document.querySelector('input[type="password"]').value;
+
+if(username === "admin" && password === "1234") {
+    alert("Login Successful!");
+    window.location.href = "index.html";
+} else {
+    alert("Invalid Username or Password");
+}
+
+}
